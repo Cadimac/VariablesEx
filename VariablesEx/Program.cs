249bf0ee06d36e3d32ca04f087599b651e9b0b25
Stephen Taylor <S.Taylor2652@gmail.com>
2022-03-18 14:53:03 -0400

    New Changes

---
+++ VariablesEx/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariablesEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Stephen";
+            int myAge =34;
+            char myInitial = 's';
+            bool isBored = false;
+            double favoriteNumber = 3.14;
+            decimal secondFavoriteNumber = 3.1415m;
+
+
+            Console.WriteLine($"My name is {myName}, My age  is {myAge}. The first letter of my name is {myInitial}. Half of the time {isBored}. My favorite numbers are {favoriteNumber}, and {secondFavoriteNumber}.");
+
+
+        }
+    }
+}
